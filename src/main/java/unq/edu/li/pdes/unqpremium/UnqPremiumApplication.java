14d1f20b0957bf870e1751302a21694f04a5ec57
zolezzi <cezcardozo23@gmail.com>
2023-03-16 22:12:57 -0300

    Adding project skeleton

---
+++ src/main/java/unq/edu/li/pdes/unqpremium/UnqPremiumApplication.java
@@ -0,0 +1,13 @@
+package unq.edu.li.pdes.unqpremium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnqPremiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnqPremiumApplication.class, args);
+	}
+
+}
