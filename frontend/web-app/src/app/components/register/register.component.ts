ca70ce178e7da7089e286d9c3ae783c6f806750b
escobarGisele <escobargisele1@gmail.com>
2023-04-09 02:17:21 -0300

    #18 #19

---
+++ frontend/web-app/src/app/components/register/register.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+interface Roles {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  registerForm!: FormGroup;
+
+  onSubmit() {}
+}
---
+++ frontend/web-app/src/app/components/semester/semester.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-semester',
+  templateUrl: './semester.component.html',
+  styleUrls: ['./semester.component.css']
+})
+export class SemesterComponent {
+
+}
