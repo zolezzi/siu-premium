b8521f46120848d1956b3220bd9601827b58d86a
zolezzi <cezcardozo23@gmail.com>
2023-04-09 23:23:24 -0300

    [Frontend] Added new script swagger generate and genere service and model

---
+++ frontend/web-app/src/app/api/api.ts
@@ -0,0 +1,11 @@
+export * from './service/committeeController.service';
+import { CommitteeControllerService } from './service/committeeController.service';
+export * from './service/degreeController.service';
+import { DegreeControllerService } from './service/degreeController.service';
+export * from './service/semesterController.service';
+import { SemesterControllerService } from './service/semesterController.service';
+export * from './service/subjectController.service';
+import { SubjectControllerService } from './service/subjectController.service';
+export * from './service/userController.service';
+import { UserControllerService } from './service/userController.service';
+export const APIS = [CommitteeControllerService, DegreeControllerService, SemesterControllerService, SubjectControllerService, UserControllerService];
