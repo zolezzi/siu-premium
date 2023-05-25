61b13b999c886337d817b8f1e7c2e97d4c87e571
zolezzi <cezcardozo23@gmail.com>
2023-05-25 09:40:34 -0300

    [Backend] Added new configuration script SQL

--- src/main/resources/db/migration/V1__initial_setup.sql
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -1,8 +1,20 @@
-drop table if exists degree;
+drop table if exists committee_accounts_students;
+drop table if exists committee_accounts_professor;
+drop table if exists account;
+drop table if exists user;
+drop table if exists committee;
+drop table if exists semester_degree_subject;
 drop table if exists subject;
+drop table if exists degree;
 
+create table account(id bigint primary key AUTO_INCREMENT, account_role varchar(255) not null, dni varchar(255) not null, firstname varchar(255) not null, lastname varchar(255) not null );
+create table user(id bigint primary key AUTO_INCREMENT, email varchar(255) not null, password varchar(255) not null, account_id bigint, constraint fk_user_account FOREIGN KEY (account_id) REFERENCES account(id));
 create table degree(id bigint primary key AUTO_INCREMENT, name varchar(255) not null);
 create table subject(id bigint primary key AUTO_INCREMENT, name varchar(255) not null, is_actived boolean not null default 1, degree_id bigint, constraint fk_subject_degree FOREIGN KEY (degree_id) REFERENCES degree(id));
+
+
+INSERT INTO account(account_role, dni, firstname, lastname) VALUES ('ADMIN', '36001205', 'ADMIN', 'UNQ-PREMIUM');
+INSERT INTO user (email, password, account_id) VALUES ('adminunqpremium@gmail.com', 'SG9sYVBsYW5ldGExMSE=', 1);
 
 INSERT INTO degree(name) values ('Licenciatura en Informatica');
 INSERT INTO degree(name) values ('Licenciatura en Bioinformatica');
